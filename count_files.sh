91ca59dcb7185cfb4f78030bf4b14102bae3eac9
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-20 10:12:22 +0200

    Ajout d'un espace

--- count_files.sh
+++ count_files.sh
@@ -2,7 +2,7 @@
 #demander d'entrer le chemin du repertoire
 read myfolder
 #compter le nombres de fichiers dans le repertoire
-file_count_calc=$(ls "$myfolder"|wc -l)
+file_count_calc=$(ls "$myfolder"| wc -l)
 num_files=$(echo "$file_count_calc")
 echo ""
 echo "Le dossier $myfolder contient $num_files fichier(s)."
